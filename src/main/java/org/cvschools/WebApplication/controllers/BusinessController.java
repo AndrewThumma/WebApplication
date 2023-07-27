e0c6fa4925ae8760709f626ab07e612a546bb627
Andrew Thumma <andrewthumma@gmail.com>
2023-07-27 14:44:00 -0400

    upload form and supporting components

--- src/main/java/org/cvschools/WebApplication/controllers/BusinessController.java
+++ src/main/java/org/cvschools/WebApplication/controllers/BusinessController.java
@@ -1,13 +1,21 @@
 package org.cvschools.WebApplication.controllers;
 
+import org.cvschools.WebApplication.models.UploadForm;
+import org.cvschools.WebApplication.services.ExcelService;
+import org.cvschools.WebApplication.utilities.ExcelHelper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BusinessController {
     
+    @Autowired
+    ExcelService fileService;
+
     public Boolean fileUploaded;
 
     @GetMapping("/403b")
@@ -19,10 +27,29 @@ public class BusinessController {
     }
 
     @PostMapping("/403b")
-    public String post403b(Model model){
-        fileUploaded = true;
-
-
+    public String post403b(Model model, @ModelAttribute UploadForm uploadForm){
+        //check to verify that file is an excel file
+        if (ExcelHelper.hasExcelFormat(uploadForm.getUploadFile())){
+            
+            //if excel file attempt to save
+            try{
+                fileService.save(uploadForm.getUploadFile());
+                fileUploaded = true;
+                
+                model.addAttribute("fileUploaded", fileUploaded);
+                return "403b";
+            } catch (Exception e) {
+                fileUploaded = false;
+                
+                model.addAttribute("error", "Could Not Upload the File");
+                model.addAttribute("fileUploaded", fileUploaded);
+                return "403b";
+            }            
+        }
+        
+        //if not an excel file return error
+        fileUploaded = false;
+        model.addAttribute("error", "Please upload an excel file!");
         model.addAttribute("fileUploaded", fileUploaded);
         return "403b";
     }
